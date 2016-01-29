cbb4c0cde7f0f757efce949cfc8cd1337907b434
romagnolid <dario.romagnoli87@gmail.com>
2016-01-29 13:11:39 +0100

    changed snp detection method

--- combact.py
+++ combact.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-from Bio.Blast import NCBIXML
 import argparse
 import os
 import os.path
 import sys
 import csv
 import time
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Blast import NCBIXML
 
-def SNP_coding(x,y,report_silent=False):
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+from Bio.Data.CodonTable import TranslationError
+from itertools import groupby
+from operator import itemgetter
+
+def snp_coding_old(x,y,print_silent=False):
     """Return string differences as SNP mutation at protein level"""
     bases = ['T', 'C', 'A', 'G']
     codons = [a+b+c for a in bases for b in bases for c in bases]
@@ -33,7 +39,7 @@ def SNP_coding(x,y,report_silent=False):
                 a = codon_table.get(x_codons[i],"X")
                 b = codon_table.get(y_codons[i],"X")
 
-            if a == b != "X" and report_silent:
+            if a == b != "X" and print_silent:
                 mutations.append("{}{}{}".format(a, i+1, b))
             elif a != b == "*":
                 mutations.append("{}{}{}".format(a, i+1, b))
@@ -41,8 +47,12 @@ def SNP_coding(x,y,report_silent=False):
                 mutations.append("{}{}{}".format(a, i+1, b))
     return(";".join(mutations))
 
-def SNP_non_coding(x,y):
-    """Return string differences as SNP mutation at DNA level"""
+def snp_non_coding_old(x,y,k=1):
+    """Return string differences as SNP mutation at DNA level.
+    x = seq1
+    y = seq2
+    k = starting nucleotide default = 1"""
+
     mutations = []
     x = x.upper()
     y = y.upper()
@@ -52,17 +62,67 @@ def SNP_non_coding(x,y):
             mutated = True
             j = i
             i += 1
-            while mutated and i<len(x):
+            while i<len(x) and mutated:
                 if x[i] != y[i]:
                     i += 1
                 else:
-                    mutations.append("{}{}>{}".format(j+1,x[j:i],y[j:i]))
+                    mutations.append("{}{}>{}".format(j+k,x[j:i],y[j:i]))
                     mutated = False
                     i += 1
-            if i == len(x):
-                mutations.append("{}{}>{}".format(j+1,x[j:i],y[j:i]))
+            if i == len(x) and mutated:
+                mutations.append("{}{}>{}".format(j+k,x[j:i],y[j:i]))
         else:
             i += 1
+    return(";".join(mutations))
+
+def snp_coding(x,y,k=1,print_silent=False):
+    """Return string differences as SNP mutation at protein level"""
+
+    mutations = []
+    # carefull don't use cds option: it removes stop codon *;
+    # it throws error both if start is not start codon or last is not end codon
+    # better look by yourself if first codon is start codon ecc.
+    x_amino = Seq(x,generic_dna).translate(table=11)
+    y_amino = Seq(y,generic_dna).translate(table=11)
+
+    x_codons = [x[i:i+3] for i in range(0, len(x)-3+1, 3)]
+    y_codons = [y[i:i+3] for i in range(0, len(y)-3+1, 3)]
+
+    indexes = [i for i in range(len(x_codons)) if x_codons[i] != y_codons[i]]
+
+    for h, g in groupby(enumerate(indexes), lambda (i,x): i-x):
+        ind = map(itemgetter(1), g)
+
+        # silent
+        for i in ind: 
+            if x_amino[i] == y_amino[i] and print_silent:
+                mutations.append("{x}{i}{y}".format(i=i+k, x=x_amino[i],y=y_amino[i]))
+            else:
+                mutations.append("{x}{i}{y}".format(i=i+k, x=x_amino[i],y=y_amino[i]))
+
+    return(";".join(mutations))
+
+def snp_non_coding(x,y,k=1):
+    """Return string differences as SNP mutation at DNA level.
+    x = seq1
+    y = seq2
+    k = starting nucleotide default = 1"""
+
+    mutations = []
+    x = x.upper()
+    y = y.upper()
+    indexes = [i for i in range(len(x)) if x[i] != y[i]]
+
+    for h, g in groupby(enumerate(indexes), lambda (i,x): i-x):
+        i = map(itemgetter(1), g)
+        if len(i) == 1:
+            # e.g. "66T>G"
+            mutations.append(
+                "{i}{x}>{y}".format(i=i[0]+k,x=x[i[0]],y=y[i[0]]))
+        else:
+            # e.g. "76_77delinsTT"
+            mutations.append(
+                "{i}_{j}delins{y}".format(i=i[0]+k,j=i[-1]+k,x=x[i[0]:i[-1]],y=y[i[0]:i[-1]]))
     return(";".join(mutations))
 
 def insertion(x,y):
@@ -129,7 +189,7 @@ def main(argv=None):
     parser.add_argument("-i","--inlist",metavar="GENOMES_LIST",default="inlist.txt",
         help="list of genomes used to make the database [default=\"inlist.txt\"]")
     parser.add_argument("--silent_mut", action="store_true",
-        help="additionally report silent mutations")
+        help="additionally print silent mutations")
     parser.add_argument("-L","--length",metavar="CUTOFF",default=70,type=float,dest="len_cutoff",
         help="percent alignment length cutoff [default=70]")
     parser.add_argument("-I","--identity",metavar="CUTOFF",default=80,type=float,dest="id_cutoff",
@@ -187,31 +247,30 @@ def main(argv=None):
                 s_seq = hsp.sbjct
                 # wild-type
                 if q_len == align_len and identity == 100:
-                    #print("wt")
                     full_hits[sbjct] = full_hits.get(sbjct,[]) + ["wt"]
                     nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + ["wt"]
                     amino_hits[sbjct] = amino_hits.get(sbjct,[]) + ["wt"]
                 # SNP
                 elif q_len == align_len and gaps == 0 and identity > args.id_cutoff:
-                    #print("snp")
-                    non_coding = SNP_non_coding(q_seq,s_seq)
-                    nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + ["c.["+non_coding+"]"]
+                    non_coding = snp_non_coding(q_seq,s_seq)
+                    nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + ["c.[{}]".format(non_coding)]
                     iscds = ("CDS" in query)
                     if iscds:
-                        coding = SNP_coding(q_seq,s_seq,True)
+                        if len(q_seq)%3!=0 or len(s_seq)%3!=0:
+                            print(query,"length is not multiple of three")
+                        coding = snp_coding(q_seq,s_seq,True)
                         full_hits[sbjct] = full_hits.get(sbjct,[]) + ["c.["+non_coding+"]"+"="+"p.["+coding+"]"]
                         if args.silent_mut is True:
                             amino_hits[sbjct] = amino_hits.get(sbjct,[]) + ["p.["+coding+"]"]
                         else:
-                            coding = SNP_coding(q_seq,s_seq,False)
+                            coding = snp_coding(q_seq,s_seq,False)
                             if len(coding)>0:
                                 amino_hits[sbjct] = amino_hits.get(sbjct,[]) + ["p.["+coding+"]"]
                     else:
-                        full_hits[sbjct] = full_hits.get(sbjct,[]) + ["c.["+non_coding+"]"]
+                        full_hits[sbjct] = full_hits.get(sbjct,[]) + ["c.[{}]".format(non_coding)]
                         amino_hits[sbjct] = amino_hits.get(sbjct,[]) + ["mutant"]
                 # indel
                 elif q_start == 1 and q_end == q_len and identity > args.id_cutoff: # and gaps > 0 (implicit)
-                    #print("indel")
                     ins = insertion(q_seq,s_seq)
                     dels = deletion(q_seq,s_seq)
 
@@ -230,20 +289,24 @@ def main(argv=None):
                     full_hits[sbjct] = full_hits.get(sbjct,[]) + [frag]
                     nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + [frag]
                     amino_hits[sbjct] = amino_hits.get(sbjct,[]) + [frag]
-                # fragment WT
-                elif q_start > 1 or q_end < q_len :
-                    pass
-                    #frag = "Fragment_mut[{}:{}]".format(q_start,q_end)
-                    #full_hits[sbjct] = full_hits.get(sbjct,[]) + [frag]
-                    #nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + [frag]
-                    #amino_hits[sbjct] = amino_hits.get(sbjct,[]) + [frag]
+
+                # fragment Mutant
+                elif (q_start > 1 or q_end < q_len) and align_len/q_len*100 > args.len_cutoff:
+                    # snps only
+                    if gaps == 0:
+                        #print(query)
+                        non_coding = snp_non_coding(q_seq,s_seq,k=q_start)
+                        nucl_hits[sbjct] = nucl_hits.get(sbjct,[]) + ["fragment[{}:{}]_c.[{}]".format(q_start,q_end,non_coding)]
+                        full_hits[sbjct] = full_hits.get(sbjct,[]) + ["fragment[{}:{}]_c.[{}]".format(q_start,q_end,non_coding)]
+                    else:
+                        pass
 
                 elif align_len/q_len*100 <= args.len_cutoff or identity <= args.id_cutoff:
-                    #print("false_positive")
                     pass
 
                 # any unforseen mutation
                 else:
+                    # for debuggin purposes
                     print("Error: unknown mutation between",query,"and",sbjct,file=sys.stderr)
                     sys.exit()
 
