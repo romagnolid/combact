6e640ae79a26ccbf0ee91695509c473e7e5de7f9
romagnolid <dario.romagnoli87@gmail.com>
2017-12-06 14:35:29 +0100

    changed output, set database name option

--- blastdb_and_blastn.py
+++ blastdb_and_blastn.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
+
 from __future__ import print_function
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from Bio.Blast.Applications import NcbiblastnCommandline
 import argparse
 import os
 import os.path
@@ -12,62 +12,65 @@ import sys
 import csv
 
 def main(argv=None):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="ComBact-0.8")
     parser.add_argument("fasta", metavar="INPUT_FILE",
         help="multifasta with genes of interest")
     parser.add_argument("output", metavar="OUTPUT_FILE",
-        help="blast output filename (format xml)")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-g", "--genomes", metavar="INPUT_GENOMES", nargs="+",
-        help="files that will be used to make the database")
-    group.add_argument("-i", "--inlist", metavar="INPUT_LIST",
-        help="list of absolute paths of files that will be used to make the database")
+        help="blast output filename (with out extention)")
+    parser.add_argument("-i", "--input_list", metavar="INPUT_LIST", required = True, 
+        help="list of absolute file paths of genomes to be used as database")
+    parser.add_argument("-d", "--database", metavar="DATABASE_DIRECTORY",
+        default = "database",
+        help="directory where database will be stored (default = 'database'")
     parser.add_argument("--makeblastdb", type=str, default="",
         help="additional options for makeblastdb (between quotes \"\")")
     parser.add_argument("--blastn", type=str, default="",
         help="additional options for blastn (between quotes \"\")")
     args = parser.parse_args(argv)
 
-    if args.genomes:
-        # create inlist to be used later
-        paths = args.genomes
+    with open(args.input_list) as infile:
+        paths = [line.split()[0] for line in infile]
         names = [os.path.splitext(os.path.basename(path))[0] for path in paths]
         genomes = zip(paths, names)
-        with open("inlist.txt", "w") as infile:
-            infile.writelines(["{string}\n".format(string=name) for name in names])
-    elif args.inlist:
-        with open(args.inlist) as infile:
-            paths = [line.split()[0] for line in infile]
-            names = [os.path.splitext(os.path.basename(path))[0] for path in paths]
-            genomes = zip(paths, names)
 
     # create database directory
-    database = os.path.join(os.getcwd(), "Database", "database.fa")
-    if not os.path.isdir("Database"):
-        os.mkdir("Database")
+    database = os.path.join(os.getcwd(), args.database, 
+        os.path.basename(args.database) + ".fa")
+    if not os.path.isdir(args.database):
+        os.mkdir(args.database)
 
     # concatenate all fastas
     sequences = []
     for path in paths:
-        genomename = os.path.splitext(os.path.basename(path))[0]
         records = SeqIO.parse(path, "fasta")
         for record in records:
-            sequences.append(SeqRecord(record.seq, id=record.id,
-                description=genomename))
+            sequences.append(SeqRecord(record.seq, id=record.description,
+            description=""))
     SeqIO.write(sequences, database, "fasta")
 
     # make blast database
-    cline = ["makeblastdb", "-parse_seqids", "-dbtype", "nucl", "-in", database, "-out", os.path.splitext(database)[0]]
+    cline = ["makeblastdb", "-parse_seqids", "-dbtype", "nucl", "-in", database,
+        "-out", os.path.splitext(database)[0]]
     print(" ".join(cline + args.makeblastdb.split()))
     out = subprocess.call(cline + args.makeblastdb.split())
 
     # blast multifasta
     start = time.time()
     print("\nBlast alignment, current time:", time.strftime("%d/%m/%y %H:%M:%S"))
-    cline = ["blastn", "-query", args.fasta, "-db", os.path.splitext(database)[0], "-outfmt", "5", "-out", args.output]
+
+    # output format asn
+    cline = ["blastn", "-query", args.fasta, "-db", os.path.splitext(database)[0],
+        "-outfmt", "11", "-out", args.output + ".asn"]
     print(" ".join(cline + args.blastn.split()))
     out = subprocess.call(cline + args.blastn.split())
-    print("Completed in", round(time.time()-start,4), "seconds.")
+
+    # output format xml (for parsing)
+    cline = ["blast_formatter", "-archive", args.output + ".asn",
+        "-outfmt", "5", "-out", args.output + ".xml"]
+    print(" ".join(cline + args.blastn.split()))
+    out = subprocess.call(cline)
+
+    print("Completed in", round(time.time()-start, 4), "seconds.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
